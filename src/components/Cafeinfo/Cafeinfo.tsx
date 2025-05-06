9fcc3701a4cbbe41d50791f956d0833d43d78078
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-05-06 20:46:08 +0300

    hw2

---
+++ src/components/Cafeinfo/Cafeinfo.tsx
@@ -0,0 +1,11 @@
+import css from "./Cafeinfo.module.css"
+
+const Cafeinfo = () => {
+    return (
+        <div className={css.container}>
+            <h1 className={css.title}>Sip Happens Café </h1>
+            <p className={css.description}>Please leave your feedback about our service by selecting one of the options below.</p>
+   </div>
+    )
+}
+export default Cafeinfo
